3cac51f763fcaa9eaa574248532b5747bcccc4cb
theartofnonso <slimbion@gmail.com>
2023-10-20 16:06:41 +0100

    Update procedure_widget.dart

--- lib/widgets/routine/editor/procedure_widget.dart
+++ lib/widgets/routine/editor/procedure_widget.dart
@@ -157,7 +157,7 @@ class ProcedureWidget extends StatelessWidget {
       padding: const EdgeInsets.all(12),
       decoration: BoxDecoration(
         color: tealBlueLight, // Set the background color
-        borderRadius: BorderRadius.circular(10), // Set the border radius to make it rounded
+        borderRadius: BorderRadius.circular(5), // Set the border radius to make it rounded
       ),
       child: Column(
         children: [
