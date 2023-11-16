07373691a3fd5ea9f8cd3cfa2442336a94607f3f
theartofnonso <slimbion@gmail.com>
2023-11-16 16:03:15 +0000

    Update set_double_textfield.dart

--- lib/widgets/routine/editor/textfields/set_double_textfield.dart
+++ lib/widgets/routine/editor/textfields/set_double_textfield.dart
@@ -4,12 +4,20 @@ import 'package:google_fonts/google_fonts.dart';
 import '../../../../app_constants.dart';
 import '../../../../utils/general_utils.dart';
 
-class SetDoubleTextField extends StatelessWidget {
+class SetDoubleTextField extends StatefulWidget {
   final double value;
-  final TextEditingController editingController;
+  final UniqueKey uniqueKey;
   final void Function(double) onChanged;
 
-  const SetDoubleTextField({super.key, required this.value, required this.onChanged, required this.editingController});
+  const SetDoubleTextField({required this.value, required this.uniqueKey, required this.onChanged}) : super(key: uniqueKey);
+
+  @override
+  State<SetDoubleTextField> createState() => _SetDoubleTextFieldState();
+}
+
+class _SetDoubleTextFieldState extends State<SetDoubleTextField> {
+
+  late TextEditingController _controller;
 
   double _parseDoubleOrDefault({required bool isDefaultWeightUnit, required String value}) {
     final doubleValue = double.tryParse(value) ?? 0;
@@ -20,19 +28,31 @@ class SetDoubleTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     final defaultWeightUnit = isDefaultWeightUnit();
     return TextField(
-      controller: editingController,
-      onChanged: (value) => onChanged(_parseDoubleOrDefault(isDefaultWeightUnit: defaultWeightUnit, value: value)),
+      controller: _controller,
+      onChanged: (value) => widget.onChanged(_parseDoubleOrDefault(isDefaultWeightUnit: defaultWeightUnit, value: value)),
       decoration: InputDecoration(
           contentPadding: EdgeInsets.zero,
           fillColor: tealBlueLight,
           enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(2), borderSide: const BorderSide(color: tealBlueLight)),
-          hintText: value > 0 ? value.toString() : "-",
+          hintText: widget.value > 0 ? widget.value.toString() : "-",
           hintStyle: GoogleFonts.lato(color: Colors.white70)),
       keyboardType: const TextInputType.numberWithOptions(decimal: true),
       maxLines: 1,
       textAlign: TextAlign.center,
       style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontSize: 16),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.value.toString());
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
