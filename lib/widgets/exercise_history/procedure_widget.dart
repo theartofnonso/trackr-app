c93ceea5b8c36e4db23680e53b3023f389aa0f5e
theartofnonso <slimbion@gmail.com>
2023-12-09 03:30:21 +0000

    fixes

--- lib/widgets/exercise_history/procedure_widget.dart
+++ lib/widgets/exercise_history/procedure_widget.dart
@@ -3,7 +3,12 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:tracker_app/dtos/exercise_log_dto.dart';
 
 import '../../enums/exercise_type_enums.dart';
+import '../../utils/general_utils.dart';
 import '../helper_widgets/routine_helper.dart';
+import '../routine/preview/set_headers/duration_distance_set_header.dart';
+import '../routine/preview/set_headers/duration_set_header.dart';
+import '../routine/preview/set_headers/reps_set_header.dart';
+import '../routine/preview/set_headers/weighted_set_header.dart';
 
 class ProcedureWidget extends StatelessWidget {
   final ExerciseLogDto exerciseLog;
@@ -15,6 +20,10 @@ class ProcedureWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final exerciseString = exerciseLog.exercise.type;
+    final exerciseType = ExerciseType.fromString(exerciseString);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -25,6 +34,16 @@ class ProcedureWidget extends StatelessWidget {
               style: GoogleFonts.lato(fontWeight: FontWeight.w600, color: Colors.white.withOpacity(0.8), fontSize: 15)),
         )
             : const SizedBox.shrink(),
+        switch (exerciseType) {
+          ExerciseType.weightAndReps => WeightedSetHeader(firstLabel: weightLabel().toUpperCase(), secondLabel: 'REPS',),
+          ExerciseType.weightedBodyWeight => WeightedSetHeader(firstLabel: "+${weightLabel().toUpperCase()}", secondLabel: 'REPS',),
+          ExerciseType.assistedBodyWeight => WeightedSetHeader(firstLabel: '-${weightLabel().toUpperCase()}', secondLabel: 'REPS',),
+          ExerciseType.weightAndDistance => WeightedSetHeader(firstLabel: weightLabel().toUpperCase(), secondLabel: distanceTitle(type: ExerciseType.weightAndDistance)),
+          ExerciseType.bodyWeightAndReps => const RepsSetHeader(),
+          ExerciseType.duration => const DurationSetHeader(),
+          ExerciseType.durationAndDistance => const DurationDistanceSetHeader(),
+        },
+        const SizedBox(height: 8),
         ...setsToWidgets(type: ExerciseType.fromString(exerciseLog.exercise.type), sets: exerciseLog.sets),
       ],
     );
--- lib/widgets/routine/preview/set_rows/duration_distance_set_row.dart
+++ lib/widgets/routine/preview/set_rows/duration_distance_set_row.dart
@@ -20,10 +20,10 @@ class DurationDistanceSetRow extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         color: tealBlueLight, // Container color
-        borderRadius: BorderRadius.circular(5.0), // Radius for rounded corners
+        borderRadius: BorderRadius.circular(3.0), // Radius for rounded corners
       ),
       child: Table(
-          border: TableBorder.all(color: tealBlueDark, borderRadius: BorderRadius.circular(5)),
+          border: TableBorder.all(color: tealBlueDark, borderRadius: BorderRadius.circular(3)),
           columnWidths: const <int, TableColumnWidth>{
         0: FixedColumnWidth(60),
         1: FlexColumnWidth(1),
