6df14fb72b2c542dddeea9637e69bb1d888236d9
theartofnonso <slimbion@gmail.com>
2023-12-11 15:45:53 +0000

    Update routine_log_editor_screen.dart

--- lib/screens/editors/routine_log_editor_screen.dart
+++ lib/screens/editors/routine_log_editor_screen.dart
@@ -130,6 +130,7 @@ class _RoutineLogEditorScreenState extends State<RoutineLogEditorScreen> {
 
   void _navigateBack() {
     SharedPrefs().remove(key: SharedPrefs().cachedRoutineLogKey);
+    SharedPrefs().remove(key: SharedPrefs().cachedRoutineLogKey);
     //print("Does cached_routine_log_key exist Editor: ${sharedPref.containsKey(cachedRoutineLogKey)}");
     Navigator.of(context).pop();
   }
