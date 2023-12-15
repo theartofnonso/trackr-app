048922a58bef4e96962480a5d60b1f93a37a6208
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2023-12-15 23:52:26 +0000

    fixes

--- lib/screens/exercise/history/exercise_chart_screen.dart
+++ lib/screens/exercise/history/exercise_chart_screen.dart
@@ -1,5 +1,4 @@
 import 'package:collection/collection.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
