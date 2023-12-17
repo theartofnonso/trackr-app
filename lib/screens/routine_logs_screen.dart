fd633fc6a7d63664d7c9473ab8ced651289ea830
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2023-12-17 19:12:03 +0000

    Update routine_logs_screen.dart

--- lib/screens/routine_logs_screen.dart
+++ lib/screens/routine_logs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tracker_app/extensions/datetime_extension.dart';
+import 'package:tracker_app/widgets/empty_states/list_view_empty_state.dart';
 
 import '../../../app_constants.dart';
 import '../../../models/RoutineLog.dart';
@@ -22,14 +23,24 @@ class RoutineLogsScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
           child: Column(
             children: [
-              Expanded(
-                child: ListView.separated(
-                    padding: const EdgeInsets.only(bottom: 150),
-                    itemBuilder: (BuildContext context, int index) => _RoutineLogWidget(log: logs[index]),
-                    separatorBuilder: (BuildContext context, int index) =>
-                        Divider(color: Colors.white70.withOpacity(0.1)),
-                    itemCount: logs.length),
-              )
+              logs.isNotEmpty
+                  ? Expanded(
+                      child: ListView.separated(
+                          padding: const EdgeInsets.only(bottom: 150),
+                          itemBuilder: (BuildContext context, int index) => _RoutineLogWidget(log: logs[index]),
+                          separatorBuilder: (BuildContext context, int index) =>
+                              Divider(color: Colors.white70.withOpacity(0.1)),
+                          itemCount: logs.length),
+                    )
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const ListViewEmptyState(),
+                        const SizedBox(height: 8),
+                        Text("You have no logs",
+                            style: GoogleFonts.lato(fontWeight: FontWeight.w500, fontSize: 16, color: Colors.white70))
+                      ],
+                    ),
             ],
           ),
         ),
