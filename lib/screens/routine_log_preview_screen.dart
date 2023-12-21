9a8659bc0773f76691928ba87e9f93d4e521ea2b
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2023-12-21 15:59:50 +0000

    Update routine_log_preview_screen.dart

--- lib/screens/routine_log_preview_screen.dart
+++ lib/screens/routine_log_preview_screen.dart
@@ -341,8 +341,6 @@ class _RoutineLogPreviewScreenState extends State<RoutineLogPreviewScreen> {
     _isLatestLogForTemplate = widget.finishedLogging || Provider.of<RoutineLogProvider>(context, listen: false)
         .isLatestLogForTemplate(templateId: widget.log.routine?.id ?? "", logId: widget.log.id);
 
-    print(_isLatestLogForTemplate);
-
     if (!_isLatestLogForTemplate) {
       return;
     }
