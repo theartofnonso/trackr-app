d9f3686cff96c3c8f3f025491bb79ff7f8e2a67d
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2024-01-05 10:48:27 +0000

    Update exercise_extension.dart

--- lib/extensions/exercise_extension.dart
+++ lib/extensions/exercise_extension.dart
@@ -14,8 +14,6 @@ extension ExerciseExtension on Exercise {
     final typeJson = dataJson["type"] ?? "";
     final type = ExerciseType.fromString(typeJson);
 
-    print("ExerciseExtension: $id, $primaryMuscleGroup, $typeJson, $type");
-
     return ExerciseDto(id: id, name: name, primaryMuscleGroup: MuscleGroup.fromString(primaryMuscleGroup), type: type);
   }
 }
