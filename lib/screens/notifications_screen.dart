520eb3f4f943fe465eb3b5f297a8550b1952487d
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2024-01-09 21:32:38 +0000

    Update notifications_screen.dart

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:collection/collection.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
