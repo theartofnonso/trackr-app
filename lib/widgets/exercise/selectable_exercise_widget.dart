1cfe7920d58f5179e015980c46e483843060f8e8
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2024-01-12 20:53:10 +0000

    fixes

--- lib/widgets/exercise/selectable_exercise_widget.dart
+++ lib/widgets/exercise/selectable_exercise_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tracker_app/app_constants.dart';
 
@@ -42,23 +43,30 @@ class SelectableExerciseWidget extends StatelessWidget {
             color: Colors.white,
           ),
         ),
-        trailing: Row(children: [
-          if (exerciseInLibraryDto.exercise.owner)
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              decoration: BoxDecoration(
-                color: tealBlueLighter,
-                borderRadius: BorderRadius.circular(3),
+        horizontalTitleGap: 4,
+        trailing: SizedBox(
+          width: 100,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+            if (exerciseInLibraryDto.exercise.owner)
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                decoration: BoxDecoration(
+                  color: tealBlueLighter,
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: Text(
+                  "owner",
+                  style: GoogleFonts.montserrat(color: Colors.white70, fontWeight: FontWeight.bold, fontSize: 12),
+                ),
               ),
-              child: Text(
-                "user",
-                style: GoogleFonts.montserrat(color: Colors.white70, fontWeight: FontWeight.bold, fontSize: 12),
-              ),
-            ),
-          exerciseInLibraryDto.selected
-              ? const Icon(Icons.check_box_rounded, color: Colors.green)
-              : const Icon(Icons.check_box_rounded, color: tealBlueLighter)
-        ]),
+            const SizedBox(width: 8),
+            exerciseInLibraryDto.selected
+                ? const FaIcon(FontAwesomeIcons.solidSquareCheck, color: Colors.green)
+                : const FaIcon(FontAwesomeIcons.solidSquareCheck, color: tealBlueLighter)
+          ]),
+        ),
       ),
     );
   }
