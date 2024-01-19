2c3ddfecda34c81beef36a0aeedc966d481a50f4
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2024-01-19 13:55:45 +0000

    fixes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:tracker_app/app_constants.dart';
 import 'package:tracker_app/controllers/routine_log_controller.dart';
-import 'package:tracker_app/screens/achievements_screen.dart';
+import 'package:tracker_app/screens/achievements/achievements_screen.dart';
 import 'package:tracker_app/screens/overview_screen.dart';
 import 'package:tracker_app/screens/template/routine_templates_screen.dart';
 import 'package:tracker_app/shared_prefs.dart';
