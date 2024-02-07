f838ab90bf1b07a00351715c9a50711fd3b1f9a4
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2024-02-07 01:05:10 +0000

    Update strings.dart

--- lib/strings.dart
+++ lib/strings.dart
@@ -1,7 +1,7 @@
 const overviewMonitor =
-    "Streak is based on training sessions, "
-    "with an optimal target of 12. "
-    "Muscle is the frequency of muscle groups trained.";
+    "Streak is based on monthly training sessions, "
+    "with an optimal target of 12 days per month. "
+    "Muscle is the frequency of muscle groups trained monthly.";
 
 const exploreWorkouts =
     "Explore curated workouts, from daily splits to focusing on particular muscle groups and classic routines";
