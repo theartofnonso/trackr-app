c655ff5af62157bb4ab526b4d9d0f66f24c330af
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2024-02-14 17:00:49 +0000

    Fixes

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tracker_app/widgets/buttons/text_button_widget.dart';
-import 'package:tracker_app/widgets/empty_states/achievements_empty_state.dart';
-import 'package:tracker_app/widgets/empty_states/muscle_group_split_frequency_empty_state.dart';
 
 import '../colors.dart';
 
@@ -17,36 +15,44 @@ class IntroScreen extends StatelessWidget {
     return MaterialApp(
       theme: themeData,
       home: Scaffold(
-        body: SafeArea(
-          minimum: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 20),
-                Image.asset(
-                  'images/trackr.png',
-                  fit: BoxFit.contain,
-                  height: 16, // Adjust the height as needed
-                ),
-                const Spacer(),
-                const Column(
-                  children: [AchievementsEmptyState(), SizedBox(height: 20), MuscleGroupSplitFrequencyEmptyState()],
-                ),
-                const Spacer(),
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: CTextButton(
-                    onPressed: onComplete,
-                    label: "Start training better",
-                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 14),
-                    buttonColor: vibrantGreen,
-                    textStyle: GoogleFonts.montserrat(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.black),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                sapphireDark80,
+                sapphireDark,
+              ],
+            ),
+          ),
+          child: SafeArea(
+            minimum: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Spacer(),
+                  Image.asset(
+                    'images/trackr.png',
+                    fit: BoxFit.contain,
+                    height: 16, // Adjust the height as needed
                   ),
-                ),
-                const SizedBox(height: 10),
-              ]),
+                  const Spacer(),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: CTextButton(
+                      onPressed: onComplete,
+                      label: "Start training better",
+                      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 14),
+                      buttonColor: vibrantGreen,
+                      textStyle: GoogleFonts.montserrat(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.black),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                ]),
+          ),
         ),
       ),
     );
