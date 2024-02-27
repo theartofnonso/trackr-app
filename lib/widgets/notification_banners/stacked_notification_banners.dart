0e6d29331ae7de25da197eca4efe30585dc3cc1f
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2024-02-27 07:23:50 +0000

    Fixed

--- lib/widgets/notification_banners/stacked_notification_banners.dart
+++ lib/widgets/notification_banners/stacked_notification_banners.dart
@@ -4,7 +4,6 @@ import 'package:tracker_app/controllers/notification_controller.dart';
 import 'package:tracker_app/extensions/datetime_extension.dart';
 import 'package:tracker_app/shared_prefs.dart';
 import 'package:tracker_app/widgets/notification_banners/weekly_training_MGF_banner.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 
 class StackedNotificationBanners extends StatefulWidget {
   const StackedNotificationBanners({super.key});
@@ -21,17 +20,13 @@ class _StackedNotificationBannersState extends State<StackedNotificationBanners>
     final untrainedMGFNotification =
         notificationController.cachedNotification(key: SharedPrefs().cachedUntrainedMGFNotification);
 
-
     return Stack(alignment: Alignment.center, children: [
       if (DateTime.now()
           .withoutTime()
           .isSameDayMonthYear(untrainedMGFNotification?.dateTime ?? DateTime.now().subtract(const Duration(days: 1))))
-        Animate(
-          effects: const [FadeEffect(), ScaleEffect()],
-          child: const Padding(
-            padding: EdgeInsets.only(top: 16.0, bottom: 4),
-            child: WeeklyTrainingMGFBanner(),
-          ),
+        const Padding(
+          padding: EdgeInsets.only(top: 16.0, bottom: 4),
+          child: WeeklyTrainingMGFBanner(),
         ),
     ]);
   }
