38976baa730841a08d9672235566d7b03c4eed41
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2024-03-04 22:58:20 +0000

    Update overview_screen.dart

--- lib/screens/insights/overview_screen.dart
+++ lib/screens/insights/overview_screen.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
