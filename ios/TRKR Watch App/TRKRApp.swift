09e5d1625a86ea6316a4628aa846ea64db05efd4
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2024-04-20 22:05:56 +0100

    Fixes

---
+++ ios/TRKR Watch App/TRKRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRKRApp.swift
+//  TRKR Watch App
+//
+//  Created by Nonso Emmanuel Biose on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TRKR_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
