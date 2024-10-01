31e060950a328bda0dc9495c1370e632dd108b9f
Foodate Developer <slimbion@gmail.com>
2024-10-01 06:33:23 +0100

    Update muscle_groups_family_frequency_widget.dart

--- lib/widgets/monthly_insights/muscle_groups_family_frequency_widget.dart
+++ lib/widgets/monthly_insights/muscle_groups_family_frequency_widget.dart
@@ -43,40 +43,44 @@ class _MuscleGroupFamilyFrequencyWidgetState extends State<MuscleGroupFamilyFreq
 
     return GestureDetector(
       onTap: _onTap,
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Row(children: [
-          Text("Muscle Groups Frequency".toUpperCase(),
-              style: GoogleFonts.ubuntu(color: Colors.white70, fontSize: 12, fontWeight: FontWeight.bold)),
-          const Spacer(),
-          if (muscleGroupFamilyFrequencies.length > 3)
-            FaIcon(_minimized ? FontAwesomeIcons.angleDown : FontAwesomeIcons.angleUp, color: Colors.white70, size: 16),
+      child: Container(
+        color: Colors.transparent,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Row(children: [
+            Text("Muscle Groups Frequency".toUpperCase(),
+                style: GoogleFonts.ubuntu(color: Colors.white70, fontSize: 12, fontWeight: FontWeight.bold)),
+            const Spacer(),
+            if (muscleGroupFamilyFrequencies.length > 3)
+              FaIcon(_minimized ? FontAwesomeIcons.angleDown : FontAwesomeIcons.angleUp,
+                  color: Colors.white70, size: 16),
+          ]),
+          const SizedBox(height: 10),
+          Text(
+              "Train a variety of muscle groups to avoid muscle imbalances and prevent injury. On average each muscle group should be trained at least 2 times a week.",
+              style: GoogleFonts.ubuntu(color: Colors.white70, fontSize: 12, fontWeight: FontWeight.w500)),
+          const SizedBox(height: 10),
+          MuscleGroupFamilyFrequencyChart(frequencyData: muscleGroupFamilyFrequencies, minimized: _minimized),
+          if (untrainedMuscleGroups.isNotEmpty)
+            Column(
+              children: [
+                RichText(
+                    text: TextSpan(
+                        text: "You have not trained",
+                        style: GoogleFonts.ubuntu(color: Colors.white70, fontSize: 12, fontWeight: FontWeight.w500),
+                        children: [
+                      const TextSpan(text: " "),
+                      TextSpan(
+                          text: untrainedMuscleGroupsNames,
+                          style: GoogleFonts.ubuntu(color: Colors.white, fontSize: 12, fontWeight: FontWeight.w600)),
+                      const TextSpan(text: " "),
+                      const TextSpan(
+                          text: "this month",
+                          style: TextStyle(color: Colors.white70, fontSize: 12, fontWeight: FontWeight.w500)),
+                    ])),
+              ],
+            ),
         ]),
-        const SizedBox(height: 10),
-        Text(
-            "Train a variety of muscle groups to avoid muscle imbalances and prevent injury. On average each muscle group should be trained at least 2 times a week.",
-            style: GoogleFonts.ubuntu(color: Colors.white70, fontSize: 12, fontWeight: FontWeight.w500)),
-        const SizedBox(height: 10),
-        MuscleGroupFamilyFrequencyChart(frequencyData: muscleGroupFamilyFrequencies, minimized: _minimized),
-        if (untrainedMuscleGroups.isNotEmpty)
-          Column(
-            children: [
-              RichText(
-                  text: TextSpan(
-                      text: "You have not trained",
-                      style: GoogleFonts.ubuntu(color: Colors.white70, fontSize: 12, fontWeight: FontWeight.w500),
-                      children: [
-                    const TextSpan(text: " "),
-                    TextSpan(
-                        text: untrainedMuscleGroupsNames,
-                        style: GoogleFonts.ubuntu(color: Colors.white, fontSize: 12, fontWeight: FontWeight.w600)),
-                    const TextSpan(text: " "),
-                    const TextSpan(
-                        text: "this month",
-                        style: TextStyle(color: Colors.white70, fontSize: 12, fontWeight: FontWeight.w500)),
-                  ])),
-            ],
-          ),
-      ]),
+      ),
     );
   }
 
