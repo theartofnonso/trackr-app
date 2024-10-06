f5b0cb5b47ba4d8d16829589bf7501b08f5d95db
Foodate Developer <slimbion@gmail.com>
2024-10-06 18:55:06 +0100

    Update label_divider.dart

--- lib/widgets/label_divider.dart
+++ lib/widgets/label_divider.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../colors.dart';
-
 class LabelDivider extends StatelessWidget {
   final String label;
   final Color labelColor;
