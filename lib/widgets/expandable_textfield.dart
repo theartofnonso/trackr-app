00c790886419aac2caeeadea2d8590bef7a099b4
Foodate Developer <slimbion@gmail.com>
2024-10-08 21:01:25 +0100

    Update expandable_textfield.dart

--- lib/widgets/expandable_textfield.dart
+++ lib/widgets/expandable_textfield.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../colors.dart';
-
 class ExpandableTextFieldWidget extends StatelessWidget {
   final Function(String) onChanged;
   final Function() onClear;
