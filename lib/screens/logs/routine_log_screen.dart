4a4b4644394d6d232d0fb73dff5566f5c23079ad
Foodate Developer <slimbion@gmail.com>
2024-10-11 01:34:17 +0100

    Update routine_log_screen.dart

--- lib/screens/logs/routine_log_screen.dart
+++ lib/screens/logs/routine_log_screen.dart
@@ -554,51 +554,52 @@ class _TRKRSummaryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        shape: BoxShape.rectangle, // Use BoxShape.circle for circular borders
-        gradient: const LinearGradient(
-          colors: [Colors.blue, Colors.green], // Gradient colors
-        ),
-        borderRadius: BorderRadius.circular(5),
-      ),
+    return GestureDetector(
+      onTap: onTap,
       child: Container(
-        margin: const EdgeInsets.all(1), // Border width
+        width: double.infinity,
         decoration: BoxDecoration(
+          shape: BoxShape.rectangle, // Use BoxShape.circle for circular borders
           gradient: const LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              sapphireDark80,
-              sapphireDark,
-            ],
+            colors: [Colors.blue, Colors.green], // Gradient colors
           ),
           borderRadius: BorderRadius.circular(5),
         ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                  "Completing a workout is an achievement, however consistent progress is what drives you toward your ultimate fitness goals.",
-                  textAlign: TextAlign.left,
-                  style: GoogleFonts.ubuntu(fontWeight: FontWeight.w400, color: Colors.white, fontSize: 14)),
-              const SizedBox(height: 6),
-              GestureDetector(
-                onTap: onTap,
-                child: ShaderMask(
-                  shaderCallback: (bounds) => const LinearGradient(
-                    colors: [Colors.blue, Colors.green],
-                    tileMode: TileMode.mirror,
-                  ).createShader(Rect.fromLTWH(0.0, 0.0, bounds.width, bounds.height)),
-                  child: Text("Review your feedback",
-                      textAlign: TextAlign.left,
-                      style: GoogleFonts.ubuntu(fontWeight: FontWeight.bold, color: Colors.white70, fontSize: 14)),
-                ),
-              )
-            ],
+        child: Container(
+          margin: const EdgeInsets.all(1), // Border width
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                sapphireDark80,
+                sapphireDark,
+              ],
+            ),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                    "Completing a workout is an achievement, however consistent progress is what drives you toward your ultimate fitness goals.",
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.ubuntu(fontWeight: FontWeight.w400, color: Colors.white, fontSize: 14)),
+                const SizedBox(height: 6),
+                ShaderMask(
+                    shaderCallback: (bounds) => const LinearGradient(
+                      colors: [Colors.blue, Colors.green],
+                      tileMode: TileMode.mirror,
+                    ).createShader(Rect.fromLTWH(0.0, 0.0, bounds.width, bounds.height)),
+                    child: Text("Review your feedback",
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.ubuntu(fontWeight: FontWeight.bold, color: Colors.white70, fontSize: 14)),
+                  ),
+
+              ],
+            ),
           ),
         ),
       ),
