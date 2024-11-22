d6debe1621c8b4c515566c3014337e31e18a7df4
Foodate Developer <slimbion@gmail.com>
2024-11-22 23:43:38 +0000

    Update month_summary_widget.dart

--- lib/widgets/monthly_insights/month_summary_widget.dart
+++ lib/widgets/monthly_insights/month_summary_widget.dart
@@ -100,7 +100,7 @@ class MonthSummaryWidget extends StatelessWidget {
                     verticalAlignment: TableCellVerticalAlignment.middle,
                     child: Center(
                       child: _TableItem(
-                          title: 'Workouts'.toUpperCase(),
+                          title: 'Sessions'.toUpperCase(),
                           subTitle: "${routineLogs.length}",
                           titleColor: logStreakColor(value: routineLogs.length / 12),
                           subTitleColor: logStreakColor(value: routineLogs.length / 12),
