b1ce63c3b840b3d153255b22023f907f6e266abc
Foodate Developer <slimbion@gmail.com>
2024-12-10 00:55:23 +0000

    Update posthog_analytics_event.dart

--- lib/enums/posthog_analytics_event.dart
+++ lib/enums/posthog_analytics_event.dart
@@ -14,14 +14,14 @@ enum PostHogAnalyticsEvent {
   generateMonthlyInsights(displayName: "report:monthly_insights_generate"),
   generateMuscleGroupTrainingReport(displayName: "report:muscle_group_training_generate"),
 
+  /// Sharing
+  
   shareRoutineLogSummary(displayName: "routine_log_summary:routine_log_stats_share"),
   shareRoutineLogAsLink(displayName: "routine_log_summary:routine_log_link_copy"),
   shareRoutineLogAsText(displayName: "routine_log_summary:routine_log_text_copy"),
 
   shareRoutineTemplateAsLink(displayName: "routine_template:routine_template_link_copy"),
   shareRoutineTemplateAsText(displayName: "routine_template:routine_template_text_copy"),
-
-  /// Sharing
 
   shareCalendar(displayName: "app:calender_share"),
   shareMonitor(displayName: "app:monitor_share"),
