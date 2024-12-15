fbdbfe023a4930e3f0715e959ea8814ecfa12103
Foodate Developer <slimbion@gmail.com>
2024-12-15 10:24:44 +0000

    Update colors.dart

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,6 +4,5 @@ const sapphireLighter = Color.fromRGBO(37, 41, 54, 1);
 const sapphireLight = Color.fromRGBO(25, 28, 36, 1);
 const sapphireDark = Color.fromRGBO(12, 14, 18, 1);
 const sapphireDark80 = Color.fromRGBO(27, 33, 38, 1.0);
-const sapphireDark60 = Color.fromRGBO(31, 38, 44, 1.0);
 const vibrantGreen = Color.fromRGBO(43, 242, 12, 1);
 const vibrantBlue = Color.fromRGBO(3, 140, 236, 1.0);
