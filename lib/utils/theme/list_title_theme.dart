b5e276695e5184c03a92ff522930fbf79cd69fcf
Foodate Developer <slimbion@gmail.com>
2024-12-17 22:50:37 +0000

    Update list_title_theme.dart

--- lib/utils/theme/list_title_theme.dart
+++ lib/utils/theme/list_title_theme.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:tracker_app/colors.dart';
 
 class TRKRListTileTheme {
   TRKRListTileTheme._();
