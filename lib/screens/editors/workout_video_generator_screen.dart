3efd3c4b4a3cba7953d874ffad98c7a807893e9d
Foodate Developer <slimbion@gmail.com>
2025-01-03 15:23:49 +0000

    fixes

--- lib/screens/editors/workout_video_generator_screen.dart
+++ lib/screens/editors/workout_video_generator_screen.dart
@@ -3,6 +3,8 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tracker_app/colors.dart';
+import 'package:tracker_app/utils/dialog_utils.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 import '../../utils/general_utils.dart';
 import '../../widgets/buttons/opacity_button_widget.dart';
@@ -15,6 +17,8 @@ class WorkoutVideoGeneratorScreen extends StatefulWidget {
 }
 
 class _WorkoutVideoGeneratorScreenState extends State<WorkoutVideoGeneratorScreen> {
+  final yt = YoutubeExplode();
+
   late TextEditingController _textEditingController;
 
   @override
@@ -45,11 +49,18 @@ class _WorkoutVideoGeneratorScreenState extends State<WorkoutVideoGeneratorScree
                 spacing: 10,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  Text("Take your strength training to the next level",
+                      textAlign: TextAlign.start, style: Theme.of(context).textTheme.titleLarge?.copyWith(height: 1.5)),
+                  Text(
+                      "Let TRKR do the work for you—upload a Youtube video, and we’ll analyze it to automatically identify the exercises, making tracking seamless.",
+                      textAlign: TextAlign.start,
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(fontSize: 14, height: 1.8)),
+                  const SizedBox(height: 2),
                   TextField(
                     controller: _textEditingController,
                     cursorColor: isDarkMode ? Colors.white : Colors.black,
                     decoration: InputDecoration(
-                      hintText: "Describe Activity",
+                      hintText: "Paste a link to a workout video",
                     ),
                     keyboardType: TextInputType.text,
                     textCapitalization: TextCapitalization.words,
@@ -61,7 +72,7 @@ class _WorkoutVideoGeneratorScreenState extends State<WorkoutVideoGeneratorScree
                     width: double.infinity,
                     height: 45,
                     child: OpacityButtonWidget(
-                        onPressed: () {},
+                        onPressed: _generate,
                         label: "Create guided session",
                         buttonColor: vibrantGreen,
                         padding: const EdgeInsets.all(10.0)),
@@ -71,9 +82,69 @@ class _WorkoutVideoGeneratorScreenState extends State<WorkoutVideoGeneratorScree
     );
   }
 
+  void _generate() async {
+
+    final url = _textEditingController.text.trim();
+
+    final isValidUrl = _isYouTubeUrl(url: url);
+
+    if(!isValidUrl) {
+      showSnackbar(context: context, icon: FaIcon(FontAwesomeIcons.circleInfo), message: "Please provide a valid Youtube Url");
+      return;
+    }
+
+    final uri = Uri.parse(url);
+
+    // The video ID is in the 'v' query parameter
+    final videoId = uri.queryParameters['v'];
+    
+    final trackManifest = await yt.videos.closedCaptions.getManifest("XXVq8o7Bs5o");
+
+    final closedCaptionsTrackInfo = trackManifest.getByLanguage('en'); // Get english caption.
+print(closedCaptionsTrackInfo);
+    if (closedCaptionsTrackInfo.isNotEmpty) {
+      final trackInfo = closedCaptionsTrackInfo.first;
+
+      // Get the actual closed caption track.
+      final track = await yt.videos.closedCaptions.get(trackInfo);
+
+      final string = track.captions.map((caption) => caption.text).join(" ");
+
+      print(string);
+
+      // Get the caption displayed at 1:01
+      for (final caption in track.captions) {
+        final text = caption.text; // "And the game was afoot."
+        print(text);
+      }
+    }
+  }
+
+  bool _isYouTubeUrl({required String url}) {
+    try {
+      final uri = Uri.parse(url);
+
+      // Check if the host contains "youtube.com" or "youtu.be"
+      if (uri.host.contains('youtube.com') || uri.host.contains('youtu.be')) {
+        return true;
+      }
+    } catch (e) {
+      // If parsing fails, it's not a valid URL at all
+      return false;
+    }
+
+    return false;
+  }
+
   @override
   void initState() {
     super.initState();
     _textEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    yt.close();
+    super.dispose();
   }
 }
