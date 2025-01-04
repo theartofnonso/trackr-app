695f02ec7f321a0eaf01dd0e9874bab2ad19380c
Foodate Developer <slimbion@gmail.com>
2025-01-04 18:01:09 +0000

    Update exercise_log_widget.dart

--- lib/widgets/routine/editors/exercise_log_widget.dart
+++ lib/widgets/routine/editors/exercise_log_widget.dart
@@ -272,7 +272,7 @@ class _ExerciseLogWidgetState extends State<ExerciseLogWidget> {
     List<TextEditingController> controllers = [];
     for (final set in sets) {
       final repsController = TextEditingController(text: (set as RepsSetDto).reps.toString());
-      _repsControllers.add((repsController));
+      controllers.add((repsController));
     }
     _repsControllers.addAll(controllers);
   }
