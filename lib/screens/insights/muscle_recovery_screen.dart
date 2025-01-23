8017dc817453c29e61b52e18e5189445c7a7be38
Foodate Developer <slimbion@gmail.com>
2025-01-23 15:01:05 +0000

    Update muscle_recovery_screen.dart

--- lib/screens/insights/muscle_recovery_screen.dart
+++ lib/screens/insights/muscle_recovery_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tracker_app/colors.dart';
 import 'package:tracker_app/utils/general_utils.dart';
 import 'package:tracker_app/widgets/buttons/opacity_button_widget.dart';
 
 import '../../enums/muscle_group_enums.dart';
+import '../../widgets/information_containers/information_container_with_background_image.dart';
 
 class MuscleRecoveryScreen extends StatefulWidget {
   static const routeName = "/muscle_recovery_screen";
@@ -26,36 +28,98 @@ class _MuscleRecoveryScreenState extends State<MuscleRecoveryScreen> {
 
     return Scaffold(
         body: Container(
+            height: double.infinity,
+            width: double.infinity,
             decoration: BoxDecoration(
               gradient: themeGradient(context: context),
             ),
             child: SafeArea(
               minimum: const EdgeInsets.all(10.0),
               bottom: false,
-              child: ListView.separated(
-                  itemBuilder: (context, index) {
-                    final muscleGroupFamily = muscleGroupFamilies[index];
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      child: Row(children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Is your ${muscleGroupFamily.name} sore",
-                                style: Theme.of(context).textTheme.bodyLarge),
-                          ],
-                        ),
-                        const Spacer(),
-                        _YesNoRadioButton(
-                          onChanged: (option) {},
-                        )
-                      ]),
-                    );
-                  },
-                  separatorBuilder: (context, index) {
-                    return Divider(height: 0.3, color: Colors.grey.withValues(alpha: 0.2));
-                  },
-                  itemCount: muscleGroupFamilies.length),
+              child: Column(children: [
+                BackgroundInformationContainer(
+                    image: 'images/woman_barbell.jpg',
+                    containerColor: Colors.green.shade900,
+                    content: "Gently press and move your muscle, to feel for any tightness, tenderness, or pain.",
+                    textStyle: GoogleFonts.ubuntu(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white.withValues(alpha: 0.9),
+                    )),
+                Expanded(
+                  child: ListView.separated(
+                      itemBuilder: (context, index) {
+                        final muscleGroupFamily = muscleGroupFamilies[index];
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("Is your ${muscleGroupFamily.name} sore",
+                                        style: Theme.of(context).textTheme.bodyLarge),
+                                  ],
+                                ),
+                                const Spacer(),
+                                _YesNoRadioButton(
+                                  onChanged: (option) {},
+                                )
+                              ]),
+                              const SizedBox(height: 10),
+                              RichText(
+                                  text: TextSpan(
+                                      text: "Your ${muscleGroupFamily.name} has",
+                                      style: GoogleFonts.ubuntu(
+                                          height: 1.5,
+                                          color: Colors.white70,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500),
+                                      children: [
+                                    const TextSpan(text: " "),
+                                    TextSpan(
+                                        text: "80%",
+                                        style: GoogleFonts.ubuntu(
+                                            color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600)),
+                                    const TextSpan(text: " "),
+                                    const TextSpan(text: "recovery. You can train it today"),
+                                  ])),
+                              const SizedBox(height: 10),
+                              Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                                decoration: BoxDecoration(
+                                  color: isDarkMode ? Colors.black12 : Colors.grey.shade100,
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                child: LinearProgressIndicator(
+                                  value: 50 / 100,
+                                  backgroundColor: isDarkMode ? sapphireDark : Colors.grey.shade400,
+                                  color: vibrantGreen,
+                                  minHeight: 25,
+                                  borderRadius: BorderRadius.circular(3.0), // Border r
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return Divider(height: 0.3, color: Colors.grey.withValues(alpha: 0.2));
+                      },
+                      itemCount: muscleGroupFamilies.length),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                    width: double.infinity,
+                    child: OpacityButtonWidget(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      buttonColor: vibrantGreen,
+                      label: "Update recovery",
+                      onPressed: () {},
+                    ))
+              ]),
             )));
   }
 }
@@ -99,7 +163,6 @@ class _YesNoRadioButtonState extends State<_YesNoRadioButton> {
 
   @override
   Widget build(BuildContext context) {
-
     Brightness systemBrightness = MediaQuery.of(context).platformBrightness;
     final isDarkMode = systemBrightness == Brightness.dark;
 
@@ -109,11 +172,19 @@ class _YesNoRadioButtonState extends State<_YesNoRadioButton> {
       children: [
         OpacityButtonWidget(
             label: 'No'.toUpperCase(),
-            buttonColor: _selectedOption == YesNoOption.no ? Colors.red : isDarkMode ? Colors.grey : Colors.grey.shade200,
+            buttonColor: _selectedOption == YesNoOption.no
+                ? Colors.red
+                : isDarkMode
+                    ? Colors.grey
+                    : Colors.grey.shade200,
             onPressed: () => _onSelectOption(YesNoOption.no)),
         OpacityButtonWidget(
             label: 'Yes'.toUpperCase(),
-            buttonColor: _selectedOption == YesNoOption.yes ? vibrantGreen : isDarkMode ? Colors.grey : Colors.grey.shade200,
+            buttonColor: _selectedOption == YesNoOption.yes
+                ? vibrantGreen
+                : isDarkMode
+                    ? Colors.grey
+                    : Colors.grey.shade200,
             onPressed: () => _onSelectOption(YesNoOption.yes)),
       ],
     );
