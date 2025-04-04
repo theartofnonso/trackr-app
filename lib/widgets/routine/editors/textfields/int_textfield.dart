4f47232771a6b5bb78788584b8d5353bf3a8c7b3
Foodate Developer <slimbion@gmail.com>
2025-04-04 16:27:41 +0100

    fixes

--- lib/widgets/routine/editors/textfields/int_textfield.dart
+++ lib/widgets/routine/editors/textfields/int_textfield.dart
@@ -5,9 +5,10 @@ class IntTextField extends StatelessWidget {
   final TextEditingController controller;
   final void Function(int value) onChanged;
   final void Function() onTap;
+  final int? maxLength;
 
   const IntTextField(
-      {super.key, required this.value, required this.controller, required this.onChanged, required this.onTap});
+      {super.key, required this.value, required this.controller, required this.onChanged, required this.onTap, this.maxLength});
 
   int _parseIntOrDefault({required String value}) {
     return int.tryParse(value) ?? 0;
@@ -24,11 +25,13 @@ class IntTextField extends StatelessWidget {
       cursorColor: isDarkMode ? Colors.white : Colors.black,
       onChanged: (value) => onChanged(_parseIntOrDefault(value: value)),
       onTap: onTap,
+      maxLength: maxLength,
       decoration: InputDecoration(
           focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(2), borderSide: const BorderSide(color: Colors.transparent)),
           enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(2), borderSide: const BorderSide(color: Colors.transparent)),
+          counterText: "",
           hintText: "${value > 0 ? value : '-'}"),
       keyboardType: TextInputType.number,
       maxLines: 1,
